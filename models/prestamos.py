5b6466fa13ed8cfe044514d512d02d2d14903c5a
Rodrigo Claros <rodrigoproyecto.claros102@gmail.com>
2026-05-21 06:36:51 -0600

    Delete models directory
    Reformacion de backend

--- models/prestamos.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
-
-class Prestamo():
-    __tablename__ = "prestamos"
-
-    id_prestamo = Column(Integer, primary_key=True)
-    id_usuario = Column(Integer, ForeignKey("usuarios.id_usuario"))
-    estado = Column(String)
-    fecha_prestamo = Column(DateTime)
